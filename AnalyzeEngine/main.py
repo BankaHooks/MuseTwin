42e03accef15483c3a2f92e7f76d15727d89ecd6
Daniel Kruchkov <bankahookswork@gmail.com>
2026-07-16 19:24:31 +0400

    Fixed Searching song name script

--- AnalyzeEngine/main.py
+++ AnalyzeEngine/main.py
@@ -7,7 +7,7 @@ import pandas as pd
 #reforming data set for more flexibility for future
 df = pd.DataFrame(pd.read_csv('spotify_dataset.csv'))
 df_for_similarity_algo = df.drop(['Unnamed: 0','time_signature','track_genre','duration_ms','artists','popularity','track_id','track_name','album_name','key','mode','explicit'],axis=1).apply(pd.to_numeric)
-df_for_work_with_track = df.drop(['Unnamed: 0','time_signature','duration_ms','popularity'],axis=1)
+df_for_work_with_track = df.drop(['Unnamed: 0','track_id','time_signature','duration_ms','popularity'],axis=1)
 df_for_search = df.drop(['Unnamed: 0', 'popularity', 'duration_ms', 'explicit', 'danceability', 'energy',
                         'key', 'loudness', 'mode', 'speechiness', 'acousticness',
                         'instrumentalness', 'liveness', 'valence', 'tempo', 'time_signature',
@@ -30,12 +30,18 @@ def find_track(track_name):
     character_data = list(character_data)
     for index, i in enumerate(character_data):
         print(index+1, i)
-    if len(character_data) != 0:
+    if len(character_data) > 1:
         print('We found some artists with similar song name! Please choose what artist do you mean: ')
         artist_user_chose = character_data[int(input(f'Select an artist: from 1-{len(character_data)}: ')) - 1]
         print(f'{artist_user_chose} --- {track_name}')
-        data_list = data_list[(data_list['artists'].str.lower() == artist_user_chose.lower()) & (data_list['track_name'].str.strip().str.lower() == track_name)]
-        return data_list
+        data_list = data_list[(data_list['artists'].str.lower() == artist_user_chose.lower())]
+        print(data_list.columns)
+        return df_for_similarity_algo.loc[data_list.index]
+    elif len(character_data) == 1:
+        artist_user_chose = character_data[0]
+        print(f'{artist_user_chose} --- {track_name}')
+        data_list = data_list[(data_list['artists'].str.lower() == artist_user_chose.lower())]
+        return df_for_similarity_algo.loc[data_list.index]
     else:
         print("Ooops, it`s look like we didn`t found this song!... Sorry")
         return None
@@ -54,6 +60,7 @@ def GetUser_TrackName():
 track = find_track(GetUser_TrackName())
 #find_important_data(1)
 print(track)
+print(df_for_similarity_algo.shape)
 
 # noinspection PyTypeChecker
 # - Just for Analyze
