a1d4892f8c25fc874be90b486c0d6240559ad862
Daniel Kruchkov <bankahookswork@gmail.com>
2026-07-20 11:27:13 +0400

    In development: TF-IDF function

---
+++ TF-IDF(testing).py
@@ -0,0 +1,4 @@
+#---
+# Here will be a script for TF-IDF function. Also I will connect new dataset for work with music test;
+# We start searching for song not only for Name, but for main Text words Plan: before 07/25/2025
+#---
